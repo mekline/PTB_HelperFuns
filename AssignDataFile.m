0bd84ea081a8bb73d5272f45463619a358b1ca32
clee9714 <clee69@u.rochester.edu>
2017-06-22 15:27:03 -0400

    Made better protection against overwriting data

--- AssignDataFile.m
+++ AssignDataFile.m
@@ -3,13 +3,20 @@ function [datafilepointer] = AssignDataFile(prefix, subNo)
 % ran that subject!  Exit everything in that case...
 
 % Define filenames of input files and result file:
+
+if ~ischar(subNo)
+    substr = num2str(subNo);
+else
+    substr = subNo;
+end
+
 datafilename = strcat('Data/',prefix,'_',num2str(subNo),'.dat'); % name of data file to write to
 
 % check for existing result file to prevent accidentally overwriting
 % files from a previous subject/session (except for subject numbers > 99):
-if subNo<99 & fopen(datafilename, 'rt')~=-1
+if fopen(datafilename, 'rt')~=-1
     %fclose('all');
-    error('Result data file already exists! Choose a different subject number.');
+    error('Result data file already exists! Choose a different subject ID.');
     Closeout_PTool();
 else
     datafilepointer = fopen(datafilename,'wt'); % open ASCII file for writing
